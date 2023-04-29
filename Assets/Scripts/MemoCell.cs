21eb66f44e6f82ea4c07aea75306b0ab4d342363
Unreal02 <scott0202@kaist.ac.kr>
2023-04-29 16:27:02 +0900

    마우스 커서를 칸에 올리면 메모 숫자 연하게 표시

--- Assets/Scripts/MemoCell.cs
+++ Assets/Scripts/MemoCell.cs
@@ -16,13 +16,42 @@ public class MemoCell : MonoBehaviour
             var cellNumber = child.GetComponent<MemoCellNumber>();
             cellNumber.Init(this, i);
             _texts[i] = child.GetComponent<TMP_Text>();
-            _texts[i].enabled = false;
+            _texts[i].alpha = 0f;
+        }
+    }
+
+    private void Update()
+    {
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var mouseOffset = transform.position - mousePosition;
+        if (mouseOffset.x > -0.5 && mouseOffset.x < 0.5 && mouseOffset.y > -0.5 && mouseOffset.y < 0.5)
+        {
+            for (var i = 0; i < BoardManager.BoardSize; i++)
+            {
+                if (!_memoNumbers[i])
+                {
+                    _texts[i].alpha = 0.25f;
+                }
+            }
+        }
+        else
+        {
+            for (var i = 0; i < BoardManager.BoardSize; i++)
+            {
+                if (!_memoNumbers[i])
+                {
+                    _texts[i].alpha = 0f;
+                }
+            }
         }
     }
 
     public void OnClickNumber(int index)
     {
         _memoNumbers[index] = !_memoNumbers[index];
-        _texts[index].enabled = _memoNumbers[index];
+        if (_memoNumbers[index])
+        {
+            _texts[index].alpha = 1f;
+        }
     }
 }
